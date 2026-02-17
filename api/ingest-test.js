f230b934cb8b251d39f0ed375352658fd4f0cee1
Beleidsbank <info@liftoffmedia.nl>
2026-02-17 13:11:34 +0100

    Update ingest-test.js

--- api/ingest-test.js
+++ api/ingest-test.js
@@ -1,4 +1,9 @@
 // /api/ingest-test.js
 module.exports = (req, res) => {
-  res.status(200).json({ ok: true, msg: "ingest-test is alive" });
+  res.status(200).json({
+    ok: true,
+    hasSupabaseUrl: Boolean(process.env.SUPABASE_URL),
+    hasServiceKey: Boolean(process.env.SUPABASE_SERVICE_ROLE_KEY),
+    hasOpenAIKey: Boolean(process.env.OPENAI_API_KEY),
+  });
 };
